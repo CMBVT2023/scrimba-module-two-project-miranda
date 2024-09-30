4cf24eb0c61882a25139381fecb8b743b0acf05c
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2024-09-30 14:43:30 -0700

    Added a description for the card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,16 @@
 import React from "react"
 import starImage from "../assets/star.png";
 
+/**
+ * Card Component
+ * @component
+ * @param {string} img - Directory location of Card Photo.
+ * @param {number} rating - Card's Rating.
+ * @param {number} reviewCount - Card's Review Count.
+ * @param {string} location - Card's Location.
+ * @param {string} title - Card's Title.
+ * @param {number} price - Card's Price.
+ */
 export function Card(props) {
     // The issue that the instructor was explaining in the Scrimba video is caused by the file location of the actual DOM or webpage, index.html, not being the same as the elements that make up the webpage
     // even though the webpage has a Card element, it is being injected and thus the file location is not within src once it is loaded in the actual DOM so attempting to reference any files in the src
